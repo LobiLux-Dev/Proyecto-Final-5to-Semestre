2e2f7c7409410b2611c458f651b77233a3c5c7f5
LobiLux <luis.sanchezz.vazquezz@gmail.com>
2022-11-26 19:10:58 -0600

    creating header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,56 @@
 import './App.css'
+import { useState } from 'react'
 import { FaReact } from 'react-icons/fa'
+import { Container, Nav, Navbar } from 'react-bootstrap'
 
 const App = () => {
+  const [expanded, setExpanded] = useState(false)
+
+  const handleFocus = (event: any) =>
+    setTimeout(() => {
+      if (expanded) {
+        ;(document.querySelector('button.navbar-toggler') as HTMLElement).click()
+        setExpanded(false)
+      }
+
+      event.target.blur()
+    }, 250)
+
+  const handleToggle = () => setExpanded(!expanded)
+
   return (
     <>
-      <header></header>
+      <header>
+        <Navbar expand='sm'>
+          <Container>
+            <Navbar.Brand>
+              <Navbar.Brand href='/'>
+                <img src={process.env.PUBLIC_URL + 'logo512.png'} alt='logo' /> B-LOQ
+              </Navbar.Brand>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls='header-nav' onClick={handleToggle} />
+            <Navbar.Collapse id='header-nav'>
+              <Nav as='ul'>
+                <Nav.Item as='li'>
+                  <Nav.Link href='/about' onClick={handleFocus}>
+                    Sobre Nosotros
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item as='li'>
+                  <Nav.Link href='/books' onClick={handleFocus}>
+                    Libros
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item as='li'>
+                  <Nav.Link href='/contact' onClick={handleFocus}>
+                    Contacto
+                  </Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </header>
       <main></main>
       <footer>
         <div>
