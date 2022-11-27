a05c39ac18f1072cd1dbfb4aab8a43f205ea3908
LobiLux <luis.sanchezz.vazquezz@gmail.com>
2022-11-26 20:40:50 -0600

    adding the contact form

--- frontend/src/pages/Contact.tsx
+++ frontend/src/pages/Contact.tsx
@@ -1,11 +1,77 @@
+import '../styles/pages/Contact.css'
 import { useEffect } from 'react'
+import { Button, Form } from 'react-bootstrap'
 
 const Contact = () => {
   useEffect(() => {
     document.title = 'B-LOQ - Contact Us'
   })
 
-  return <h1>Contact Us</h1>
+  return (
+    <Form className='col-xl-4 col-lg-5 col-md-6 col-sm-8'>
+      <Form.Group controlId='name'>
+        <Form.Label>
+          Nombre <span>*</span>
+        </Form.Label>
+        <Form.Control
+          required
+          type='text'
+          title='Introduce tu Nombre'
+          placeholder='Introduce tu Nombre'
+        />
+      </Form.Group>
+      <Form.Group controlId='email'>
+        <Form.Label>
+          Email <span>*</span>
+        </Form.Label>
+        <Form.Control
+          required
+          type='email'
+          title='Introduce tu Email'
+          placeholder='Introduce tu Email'
+        />
+      </Form.Group>
+      <Form.Group controlId='phone'>
+        <Form.Label>Teléfono</Form.Label>
+        <Form.Control
+          type='tel'
+          maxLength={10}
+          title='Introduce tu Teléfono'
+          placeholder='Introduce tu Teléfono'
+        />
+      </Form.Group>
+      <Form.Group controlId='issue'>
+        <Form.Label>
+          Asunto <span>*</span>
+        </Form.Label>
+        <Form.Control
+          required
+          type='text'
+          minLength={5}
+          title='Introduce el Asunto'
+          placeholder='Introduce el Asunto (5 caracteres minimo)'
+        />
+      </Form.Group>
+      <Form.Group controlId='message'>
+        <Form.Label>
+          Mensaje <span>*</span>
+        </Form.Label>
+        <Form.Control
+          required
+          as='textarea'
+          minLength={20}
+          title='Introduce tu Mensaje'
+          placeholder='Introduce tu Mensaje (20 caracteres minimo)'
+        />
+      </Form.Group>
+      <Button variant='primary' type='submit'>
+        Enviar
+      </Button>
+      <div>
+        <span>*</span> los campos son obligatorios
+      </div>
+    </Form>
+  )
 }
 
 export default Contact
