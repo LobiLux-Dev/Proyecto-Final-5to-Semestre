367877c36701b1c7749fb0801479aec975a07976
LobiLux <luis.sanchezz.vazquezz@gmail.com>
2022-11-26 19:00:20 -0600

    creating footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,25 @@
+import './App.css'
+import { FaReact } from 'react-icons/fa'
+
 const App = () => {
-  return <h1>Hello World from React</h1>
+  return (
+    <>
+      <header></header>
+      <main></main>
+      <footer>
+        <div>
+          <h5>
+            Made with <FaReact title='React JS/TS' id='react-icon' /> by:
+          </h5>
+          <ul>
+            <li>Omar Reynoso Arellano</li>
+            <li>José Luis Sánchez Vázquez</li>
+            <li>Quetzalli Marel Hernández Alba</li>
+          </ul>
+        </div>
+      </footer>
+    </>
+  )
 }
 
 export default App
