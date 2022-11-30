02d8ba45b74b6645627fe04102e3d57be5faf6cb
LobiLux <luis.sanchezz.vazquezz@gmail.com>
2022-11-29 23:38:18 -0600

    creating the about us page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,11 +1,12 @@
 import { useEffect } from 'react'
+import '../styles/pages/About.css'
 
 const About = () => {
   useEffect(() => {
     document.title = 'B-LOQ - About Us'
   })
 
-  return <h1>About Us</h1>
+  return <video src={process.env.PUBLIC_URL + '/videos/about.mp4'} controls autoPlay muted></video>
 }
 
 export default About
