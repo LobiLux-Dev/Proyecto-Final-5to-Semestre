29c295e857e2aa9f80c7b1c2d9ba095142286db3
LobiLux <luis.sanchezz.vazquezz@gmail.com>
2022-11-30 13:17:17 -0600

    modifying the "error 404" page

--- frontend/src/pages/PageNotFound.tsx
+++ frontend/src/pages/PageNotFound.tsx
@@ -1,11 +1,22 @@
 import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/pages/PageNotFound.css'
+import { Container } from 'react-bootstrap'
 
 const PageNotFound = () => {
   useEffect(() => {
-    document.title = 'B-LOQ - Page Not Found'
+    document.title = 'B-LOQ - Página no encontrada'
   })
 
-  return <h1>Page Not Found</h1>
+  return (
+    <Container id='pagenotfound-container'>
+      <h1>404</h1>
+      <h2>Página no encontrada</h2>
+      <p>
+        Has click <Link to='/'>aqui</Link> para volver al inicio
+      </p>
+    </Container>
+  )
 }
 
 export default PageNotFound
