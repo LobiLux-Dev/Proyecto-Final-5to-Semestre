5dc21ff6e5bde249b77c0d52706bf23ef56da30d
LobiLux <luis.sanchezz.vazquezz@gmail.com>
2022-12-01 23:14:31 -0600

    moving the application to the application file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,6 @@
-import express from 'express'
+import app from './app'
 import { PORT } from './config'
-
-const app = express()
-
-app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`)
 })
-
-export default app
