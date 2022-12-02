1e639c56f71fae5c57f183dbca60bc01a8183c96
LobiLux <luis.sanchezz.vazquezz@gmail.com>
2022-12-01 23:19:02 -0600

    adding middlewares

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,13 @@
+import morgan from 'morgan'
 import express from 'express'
 
 const app = express()
 
+// Middlewares
+app.use(morgan('dev'))
+app.use(express.json())
+
+// Routes
 app.get('/', (req, res) => res.send('Hello World!'))
 
 export default app
