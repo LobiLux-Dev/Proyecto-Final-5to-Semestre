f58fde277696e7fbc0fa005e9dff0c513b618aba
LobiLux <luis.sanchezz.vazquezz@gmail.com>
2022-12-02 05:55:34 -0600

    adding the books to the frontend

--- frontend/src/pages/Books.tsx
+++ frontend/src/pages/Books.tsx
@@ -1,11 +1,48 @@
-import { useEffect } from 'react'
+import axios from 'axios'
+import '../styles/pages/Books.css'
+import { useEffect, useState } from 'react'
+import { Card, Container, Row } from 'react-bootstrap'
 
 const Books = () => {
+  const [books, setBooks] = useState([])
+
   useEffect(() => {
     document.title = 'B-LOQ - Books'
-  })
 
-  return <h1>Books</h1>
+    axios
+      .get('http://localhost:3001/api/books')
+      .then(res => {
+        setBooks(res.data)
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }, [])
+
+  return (
+    <Container id='books-container'>
+      {books.map((book: any) => (
+        <Card key={book.id}>
+          <Card.Header>
+            <img
+              src={process.env.PUBLIC_URL + '/static/images/image-not-available.png'}
+              alt='Book Cover'
+            />
+          </Card.Header>
+          <Card.Body>
+            <Card.Title>{book.title}</Card.Title>
+            <Card.Text>{book.description}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Row>
+              <Card.Text>{book.author}</Card.Text>
+              <Card.Text>{book.editorial}</Card.Text>
+            </Row>
+          </Card.Footer>
+        </Card>
+      ))}
+    </Container>
+  )
 }
 
 export default Books
