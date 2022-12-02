1cfff226ae5f25e91e901854f5505f7cc8196210
LobiLux <luis.sanchezz.vazquezz@gmail.com>
2022-12-01 22:03:04 -0600

    testing with the vercel configuration file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,11 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')))
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+)
+
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`)
