dcb9058445b69b255d7d4a7a67d6f401110893a4
LobiLux <luis.sanchezz.vazquezz@gmail.com>
2022-12-02 02:53:15 -0600

    validating data received

--- backend/src/routes/books.routes.ts
+++ backend/src/routes/books.routes.ts
@@ -10,38 +10,77 @@ import {
 
 const router = Router()
 
-router.get('/', async (req, res) => {
-  res.json(await getBooks())
+router.get('/', async (req, res, next) => {
+  try {
+    res.json(await getBooks())
+  } catch (error: any) {
+    next(error.message)
+  }
 })
 
-router.get('/:id', async (req, res) => {
-  res.json(await getBook(req.params.id))
+router.get('/:id', async (req, res, next) => {
+  try {
+    if (!parseInt(req.params.id)) throw new Error('Invalid ID')
+
+    res.json(await getBook(req.params.id))
+  } catch (error: any) {
+    next(error.message)
+  }
 })
 
-router.post('/', async (req, res) => {
-  const { isbn, author, title, editorial, printingDate, quantity } = req.body
+router.post('/', async (req, res, next) => {
+  try {
+    const { isbn, author, title, editorial, printingDate, quantity } = req.body
 
-  await addBook({ isbn, author, title, editorial, printingDate, quantity })
-  res.json({ status: 'Book saved' })
+    if (!isbn) throw new Error('ISBN is required')
+    if (!author) throw new Error('Author is required')
+    if (!title) throw new Error('Title is required')
+    if (!editorial) throw new Error('Editorial is required')
+    if (!printingDate) throw new Error('Printing Date is required')
+    if (!quantity || !parseInt(quantity)) throw new Error('Quantity is required')
+
+    await addBook({ isbn, author, title, editorial, printingDate, quantity })
+    res.json({ status: 'Book saved' })
+  } catch (error: any) {
+    next(error.message)
+  }
 })
 
-router.put('/:id', async (req, res) => {
-  const { isbn, author, title, editorial, printingDate, quantity } = req.body
+router.put('/:id', async (req, res, next) => {
+  try {
+    const { isbn, author, title, editorial, printingDate, quantity } = req.body
 
-  await updateBook(req.params.id, {
-    isbn,
-    author,
-    title,
-    editorial,
-    printingDate,
-    quantity,
-  })
-  res.json({ status: 'Book updated' })
+    if (!isbn) throw new Error('ISBN is required')
+    if (!author) throw new Error('Author is required')
+    if (!title) throw new Error('Title is required')
+    if (!editorial) throw new Error('Editorial is required')
+    if (!printingDate) throw new Error('Printing Date is required')
+    if (!quantity || !parseInt(quantity)) throw new Error('Quantity is required')
+
+    await updateBook(req.params.id, {
+      isbn,
+      author,
+      title,
+      editorial,
+      printingDate,
+      quantity,
+    })
+    res.json({ status: 'Book updated' })
+  } catch (error: any) {
+    next(error.message)
+  }
 })
 
-router.delete('/:id', async (req, res) => {
-  await deleteBook(req.params.id)
-  res.json({ status: 'Book deleted' })
+router.delete('/:id', async (req, res, next) => {
+  try {
+    if (!parseInt(req.params.id)) throw new Error('Invalid ID')
+    if (!(await getBook(req.params.id))) throw new Error('Book not found')
+
+    await deleteBook(req.params.id)
+    res.json({ status: 'Book deleted' })
+  } catch (error: any) {
+    next(error.message)
+  }
 })
 
 export default router
